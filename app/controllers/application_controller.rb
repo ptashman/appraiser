e0c660955024adc53f7f5f1e5681ca115e4c8f3e
Paul Tashman <paul.tashman@pgi.com>
2016-04-30 14:58:53 -0400

    Adds access header insertion method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+  before_filter :add_access_header
+
+  def add_access_header
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end

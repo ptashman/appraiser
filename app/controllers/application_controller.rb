2d320ca1645dcb9a0febdbf5eef6bfbf62d7b506
Paul Tashman <paul.tashman@pgi.com>
2016-04-30 16:18:46 -0400

    Adds x-requested-with to access-control-allow-headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def cors_set_access_control_headers
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
     response.headers['Access-Control-Max-Age'] = "1728000"
   end
 
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     if request.method == 'OPTIONS'
       request.headers['Access-Control-Allow-Origin'] = '*'
       request.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-      request.headers['Access-Control-Allow-Headers'] = 'X-Prototype-Version, Token, Auth-Token, Email'
+      request.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Auth-Token, Email'
       request.headers['Access-Control-Max-Age'] = '1728000'  
       render :text => '', :content_type => 'text/plain'
     end

9eeab3dd0731f7a565c20a4b2f29e2694b3990a0
Paul Tashman <paul.tashman@pgi.com>
2016-04-30 15:15:21 -0400

    Adds more headers for requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
-  before_filter :add_access_header
+  before_filter :add_access_headers
 
-  def add_access_header
+  def add_access_headers
     headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
